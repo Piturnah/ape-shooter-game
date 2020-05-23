c5e11ee81007a44fab9609b3d1c366c2e495c99f
Piturnah <peterhebden6@gmail.com>
2020-05-23 14:48:01 +0100

    Player + Camera Controllers

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector3 offsetToPlayer;
+    [SerializeField] Transform playerTransform;
+
+    private void Start() {
+        offsetToPlayer = transform.position - playerTransform.position;
+    }
+
+    private void LateUpdate() {
+        transform.position = playerTransform.position + offsetToPlayer;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] LayerMask floorLayers;
+    [SerializeField] float movementSpeed;
+
+    private void Update() {
+        ReceiveRotationInput();
+        ReceiveMovementInput();
+    }
+
+    void ReceiveMovementInput() {
+        Vector3 inputDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        Debug.Log(inputDir);
+        transform.Translate(inputDir * Time.deltaTime * movementSpeed, Space.World);
+    }
+
+    void ReceiveRotationInput() {
+
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        RaycastHit hit;
+        Vector3 mouseWorldPos = new Vector3();
+        Ray mouseRay = Camera.main.ScreenPointToRay(mouseScreenPosition);
+
+        if (Physics.Raycast(mouseRay, out hit, int.MaxValue, floorLayers)) {
+            mouseWorldPos = hit.point;
+        }
+        
+        Vector2 xzDisplacementFromMousePos = new Vector2(mouseWorldPos.x, mouseWorldPos.z) - new Vector2(transform.position.x, transform.position.z);
+        float radiansToMousePos = Mathf.Atan2(xzDisplacementFromMousePos.y, xzDisplacementFromMousePos.x);
+        transform.eulerAngles = Vector3.up * (90 - radiansToMousePos * Mathf.Rad2Deg);
+    }
+}
